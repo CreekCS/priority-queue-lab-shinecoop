956fb83362dff58aecf8abb421423bdf098059f4
Cooper Shine <166638981+shinecoop@users.noreply.github.com>
2024-11-05 03:20:19 +0000

    bruh moment

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,36 @@
 // import statements
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-public class PriorityQueueLab {
-  
+public class PriorityQueueLab{
+
+    public static void main(String[] args){
+        Scanner s = null;
+        try{
+            s = new Scanner(new File("./RandIntegers.txt"));
+        }
+        catch(FileNotFoundException e){
+            System.out.println("No file goofy");
+        }
+
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        while(s.hasNextInt()){
+            q.add(s.nextInt());
+        }
+
+        int size = q.size();
+        for(int i=0; i<size; i++){
+            System.out.println(q.remove());
+        }
+    }
+
+
+
+
+
+
 }
 
